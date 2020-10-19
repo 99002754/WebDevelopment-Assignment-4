9ca1d5a06f86edcc2ef6745188a380ac887aed4a
99002754 <72496632+99002754@users.noreply.github.com>
2020-10-19 08:57:48 +0530

    Update EX04.js

--- EX04.js
+++ EX04.js
@@ -1,6 +1,6 @@
 const app= require("express")()
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/Client.html")
+    res.sendFile(__dirname + "/client.html")
 })
 app.listen(3333, ()=>{
     console.log("Client app running at 3333");
